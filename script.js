0ae3d86c31d04ff18db1e45ac7823aba5ea5dc4c
Scott Pribe <Scottpribe@gmail.com>
2020-04-15 12:10:02 -0400

    reset button doesn't work as planned but pausing and first initial start function properly.

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
-const defaultTime = 10;
+const defaultTime = 1;
 let countDownTime = defaultTime * 60;
+let intervalFunction;
 
 const displayTimeCountdown = document.getElementById('timerContainer');
-const playButton = document.querySelector('#play')
+const playButtonFind= document.getElementById('play');
+const pauseButtonFind= document.getElementById('pause');
+const resetButtonFind= document.getElementById('reset');
+
+const playButton = document.querySelector('#play');
+const pauseButton = document.querySelector('#pause');
+const resetButton = document.querySelector('#reset');
+
 playButton.addEventListener('click', () =>{
-    setInterval(startCountDown,1000);
+   intervalFunction = setInterval(startCountDown,1000);
+
+})
+
+pauseButton.addEventListener('click', () => {
+    pauseCountDown();
+})
+
+resetButton.addEventListener('click', () => {
+    displayTimeCountdown.innerText = defaultTime;
+    clearInterval(intervalFunction);
+    intervalFunction = null;
 })
 
 function startCountDown () {
-    const minutes = Math.floor(countDownTime / 60);
+    let minutes = Math.floor(countDownTime / 60);
     let seconds = countDownTime % 60;
     seconds = seconds < 10 ? '0' + seconds:seconds;
 
@@ -17,10 +36,15 @@ function startCountDown () {
     countDownTime--;
     countDownTime = countDownTime < 0 ? 0 : countDownTime; 
 
-    document.getElementById('play').disabled = true;
+    playButtonFind.disabled = true; 
+    resetButtonFind.disabled = true;
     
 }
 
 function pauseCountDown () {
-    
+    clearInterval(intervalFunction);
+    playButtonFind.disabled = false; 
+    resetButtonFind.disabled = false;
 }
+
+
