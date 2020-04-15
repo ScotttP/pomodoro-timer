56af238f0a59530551dc94e716fcb5a20d8f625f
Scott Pribe <Scottpribe@gmail.com>
2020-04-15 15:16:56 -0400

    added session and break time button functions. need to instruct buttons to actually increase and decrease time

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 const defaultTime = 1;
+let defaultTimeDisplay = defaultTime + ":00"
 let countDownTime = defaultTime * 60;
-let intervalFunction;
+let intervalFunction = null;
 
 const displayTimeCountdown = document.getElementById('timerContainer');
 const playButtonFind= document.getElementById('play');
 const pauseButtonFind= document.getElementById('pause');
 const resetButtonFind= document.getElementById('reset');
+const increaseSessionTime = document.getElementById('increaseSessionTime')
+const increaseBreakTime = document.getElementById('increaseBreakTime')
+const decreaseSessionTime = document.getElementById('decreaseSessionTime')
+const decreaseBreakTime = document.getElementById('decreaseBreakTime')
+
 
 const playButton = document.querySelector('#play');
 const pauseButton = document.querySelector('#pause');
 const resetButton = document.querySelector('#reset');
+
+displayTimeCountdown.innerText = defaultTimeDisplay;
 
 playButton.addEventListener('click', () =>{
    intervalFunction = setInterval(startCountDown,1000);
@@ -21,9 +29,25 @@ pauseButton.addEventListener('click', () => {
 })
 
 resetButton.addEventListener('click', () => {
-    displayTimeCountdown.innerText = defaultTime;
+    displayTimeCountdown.innerText = defaultTimeDisplay;
     clearInterval(intervalFunction);
-    intervalFunction = null;
+    
+})
+
+increaseSessionTime.addEventListener('click', () => {
+    increaseOrDecreaseTime(increaseSessionTime.id);
+})
+
+increaseBreakTime.addEventListener('click', () => {
+    increaseOrDecreaseTime(increaseBreakTime.id)
+})
+
+decreaseSessionTime.addEventListener('click', () => {
+    increaseOrDecreaseTime(decreaseSessionTime.id);
+})
+
+decreaseBreakTime.addEventListener('click', () => {
+    increaseOrDecreaseTime(decreaseBreakTime.id)
 })
 
 function startCountDown () {
@@ -45,6 +69,20 @@ function pauseCountDown () {
     clearInterval(intervalFunction);
     playButtonFind.disabled = false; 
     resetButtonFind.disabled = false;
+}
+
+function increaseOrDecreaseTime (breakOrSession) {
+    if (breakOrSession == "increaseSessionTime"){
+        console.log('increase session time!');
+    }else if (breakOrSession == "increaseBreakTime"){
+        console.log('increase break time');
+    }else if (breakOrSession == "decreaseSessionTime"){
+        console.log('decrease session time');
+    }else if (breakOrSession == "decreaseBreakTime"){
+        console.log('decreease break time');
+    }else {
+        console.log ('something went wrong')
+    }
 }
 
 
