9faccc114ba9792042cd2dd64af805337273c532
Scott Pribe <Scottpribe@gmail.com>
2020-04-15 10:10:49 -0400

    button countdown functionality added

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 const defaultTime = 10;
-let time = defaultTime / 60;
+let countDownTime = defaultTime * 60;
+
 const displayTimeCountdown = document.getElementById('timerContainer');
+const playButton = document.querySelector('#play')
+playButton.addEventListener('click', () =>{
+    setInterval(startCountDown,1000);
+})
 
-setInterval (countDown,1000);
-
-function countDown () {
-    const minutes = Math.floor(time / 60);
-    let seconds = time % 60;
-
+function startCountDown () {
+    const minutes = Math.floor(countDownTime / 60);
+    let seconds = countDownTime % 60;
     seconds = seconds < 10 ? '0' + seconds:seconds;
 
     displayTimeCountdown.innerText = `${minutes}:${seconds}`;
-    time--;
+
+    countDownTime--;
+    countDownTime = countDownTime < 0 ? 0 : countDownTime; 
+
+    document.getElementById('play').disabled = true;
+    
+}
+
+function pauseCountDown () {
+    
 }
